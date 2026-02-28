66a9f6dbed07ea49c4acdab9acf5340e548d3639
hrithikhadka <hrithikhadka00@gmail.com>
2026-02-28 22:30:44 +0200

    add tabs with preview images

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
@@ -24,6 +25,40 @@ export default function Home() {
               <p className="text-sm text-muted-foreground">
                 Free forever. No credit card required
               </p>
+            </div>
+          </div>
+        </section>
+
+        {/*hero images section with tabs */}
+        <section className="border-t bg-white py-16">
+          <div className="container mx-auto px-4">
+            <div className="mx-auto max-w-6xl">
+              {/*tabs*/}
+              <div className="flex gap-2 justify-center mb-8">
+                <Button>Organize Application</Button>
+                <Button>Get Hired</Button>
+                <Button>Manage Boards</Button>
+              </div>
+              <div className="relative mx-auto mx-w-5xl overflow-hidden rounded-lg border border-gray-200 shadow-xl">
+                <Image
+                  src="/hero-images/hero1.png"
+                  alt="Organize Application"
+                  width={1200}
+                  height={800}
+                />
+                <Image
+                  src="/hero-images/hero2.png"
+                  alt="Get Hired"
+                  width={1200}
+                  height={800}
+                />
+                <Image
+                  src="/hero-images/hero3.png"
+                  alt="Manage Boards"
+                  width={1200}
+                  height={800}
+                />
+              </div>
             </div>
           </div>
         </section>
